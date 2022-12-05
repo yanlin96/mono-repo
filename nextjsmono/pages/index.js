b9e406b32d916e255749721300b96914a4e813cf
“yanlin96” <yanlin96.gong@gmail.com>
2022-12-06 08:43:25 +1100

    text only

--- nextjsmono/pages/index.js
+++ nextjsmono/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! - change - tst</a>
+          Welcome to <a href="https://nextjs.org">Next.js! - change - tst - text only</a>
         </h1>
 
         <p className={styles.description}>
